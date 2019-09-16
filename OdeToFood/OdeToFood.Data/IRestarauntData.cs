7d5a075ee9d30fc3788a830a6cf57c2a90b250f7
Anatoly Koplyk <anatolykoplykgit@gmail.com>
2019-09-16 16:10:54 +0300

    Added Details view. Reorganized folder structure

---
+++ OdeToFood/OdeToFood.Data/IRestarauntData.cs
@@ -0,0 +1,39 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Anatolii`s Pizza", Location = "Kharkov", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Crishna", Location = "Odessa", Cuisine = CuisineType.Indian },
+                new Restaurant { Id = 3, Name = "The Varenik", Location = "Kyiv", Cuisine = CuisineType.Ukrainian }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return restaurants
+                .Where(r => string.IsNullOrEmpty(name) || r.Name.StartsWith(name))
+                .OrderBy(r => r.Name);
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+    }
+}
