5313945c847e36ff55824f66836f1a1b790f99c2
Anatoly Koplyk <anatolykoplykgit@gmail.com>
2019-09-18 18:46:12 +0300

    Logging Application Messages

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,13 +4,15 @@ using Microsoft.Extensions.Configuration;
 using OdeToFood.Core;
 using OdeToFood.Data;
 using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
-        private readonly IConfiguration config;
-        private readonly IRestaurantData restarauntData;
+        private readonly IConfiguration _config;
+        private readonly IRestaurantData _restaurantData;
+        private readonly ILogger<ListModel> _logger;
 
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
@@ -18,17 +20,20 @@ namespace OdeToFood.Pages.Restaurants
         [BindProperty(SupportsGet =true)]
         public string SearchTerm { get; set; }
 
-        public ListModel(IConfiguration config, IRestaurantData restarauntData)
+        public ListModel(IConfiguration config,
+            IRestaurantData restaurantData, 
+            ILogger<ListModel> logger)
         {
-            this.config = config;
-            this.restarauntData = restarauntData;
+            _config = config;
+            _restaurantData = restaurantData;
+            _logger = logger;
         }
 
         public void OnGet()
         {
-            //HttpContext.Request.Query
-            Message = config["Message"];
-            Restaurants = restarauntData.GetRestaurantsByName(SearchTerm);
+            _logger.LogError("TEST !!! Executing ListModel");
+            Message = _config["Message"];
+            Restaurants = _restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
 }
